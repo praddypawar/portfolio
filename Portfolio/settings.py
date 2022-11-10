3b1c9fdc847b55be64d5c342a5e842af54f837e0
praddypawar <115936213+praddypawar@users.noreply.github.com>
2022-11-10 12:41:52 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l(x%xj#x!463(6o%r#e(_))*6d0kv24c$s1oo4j$na3&(4j5&c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -116,13 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,"static")
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR,"static")
 
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
