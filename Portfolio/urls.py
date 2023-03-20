52e9899c4433d47bbc10f6c6e93385c3caa706de
praddypawar <115936213+praddypawar@users.noreply.github.com>
2023-03-20 16:08:07 +0530

    Update urls.py

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
     path("",include("backend.urls")),
     url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+#+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
