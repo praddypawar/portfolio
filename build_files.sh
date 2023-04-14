264f8b93c3a6406fc494585d38a29a3c7028015c
praddypawar <115936213+praddypawar@users.noreply.github.com>
2023-04-14 13:54:14 +0530

    Create build_files.sh

---
+++ build_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Build Django app
+python manage.py migrate
+python manage.py compilemessages
+python manage.py collectstatic --noinput
+
+# Deploy to Vercel
+vercel --prod
